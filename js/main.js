ffb38f3692ecfc769cdf53248e6169df045beacd
ellijayne <ellijayne.m.willard@gmail.com>
2018-11-11 20:03:00 +1100

    commented out jquery and made animation with css instead. also figured out how to put hover state on body but only animate one div

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-  $('.canvas-container').click(function() {
-
-  })
-});
+// $(document).ready(function() {
+//
+//   $('.canvas-container').mouseover(function() {
+//     console.log('mouse entered');
+//     $("div.koala__eye--lid-close").toggleClass("koala__eye--lid-close-ani");
+//   })
+//
+//   $('.canvas-container').mouseout(function() {
+//     console.log('mouse left');
+//     $("div.koala__eye--lid-close").toggleClass("koala__eye--lid-close-ani");
+//   })
+//
+// }); //end of document ready.
